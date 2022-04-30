bc8afbb86a8bdee81e3f9c54187e189f2688f3e4
GRahul007 <gadhe.rahul717@gmail.com>
2022-04-30 14:41:21 +0530

    this is my var file

---
+++ var.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+default = "AKIAYVQOFFV7OH6F7WOL"
+}
+variable "aws_secret_key" {
+default = "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"
+}
+variable "aws_region" {
+default = "us-east-2"
+}
